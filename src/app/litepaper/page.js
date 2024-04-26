c80e2cd7fdaf1261dc8e4f109c8a82dba42febaf
Incredible <incredible@pro.com>
2024-04-26 15:37:19 +0800

    [Tomislav] add keys

--- src/app/litepaper/page.js
+++ src/app/litepaper/page.js
@@ -74,8 +74,8 @@ const Litepaper = () => {
 		if (!navigationData.prev) {
 			return <button className='page_navigationBtn' style={{ width: '100%' }} onClick={() => getPageContent(navigationData.next)}>
 				<div style={{ width: '70%', textAlign: 'left' }}>
-					<p className='text-black' style={{ fontSize: 14 }}>Next</p>
-					<p className='text-black' style={{ fontSize: 22 }}>{navigationData.next.title}</p>
+					<p key={`preview_title`} className='text-black' style={{ fontSize: 14 }}>Next</p>
+					<p key={`preview_content`} className='text-black' style={{ fontSize: 22 }}>{navigationData.next.title}</p>
 				</div>
 				<p className='text-black' style={{ fontSize: 22 }}>{`>`}</p>
 			</button>
@@ -83,8 +83,8 @@ const Litepaper = () => {
 			return <button className='page_navigationBtn' style={{ width: '100%' }} onClick={() => getPageContent(navigationData.prev)}>
 				<p className='text-black' style={{ fontSize: 22 }}>{`<`}</p>
 				<div style={{ width: '70%', textAlign: 'right' }}>
-					<p className='text-black' style={{ fontSize: 14 }}>Previous</p>
-					<p className='text-black' style={{ fontSize: 22 }}>{navigationData.prev.title}</p>
+					<p key={`preview_title`} className='text-black' style={{ fontSize: 14 }}>Previous</p>
+					<p key={`preview_content`} className='text-black' style={{ fontSize: 22 }}>{navigationData.prev.title}</p>
 				</div>
 			</button>
 		} else {
@@ -92,14 +92,14 @@ const Litepaper = () => {
 				<button className='page_navigationBtn' onClick={() => getPageContent(navigationData.prev)}>
 					<p className='text-black' style={{ fontSize: 22 }}>{`<`}</p>
 					<div style={{ width: '70%', textAlign: 'right' }}>
-						<p className='text-black' style={{ fontSize: 14 }}>Previous</p>
-						<p className='text-black' style={{ fontSize: 22 }}>{navigationData.prev.title}</p>
+						<p key={`preview_title`} className='text-black' style={{ fontSize: 14 }}>Previous</p>
+						<p key={`preview_content`} className='text-black' style={{ fontSize: 22 }}>{navigationData.prev.title}</p>
 					</div>
 				</button>
 				<button className='page_navigationBtn' onClick={() => getPageContent(navigationData.next)}>
 					<div style={{ width: '70%', textAlign: 'left' }}>
-						<p className='text-black' style={{ fontSize: 14 }}>Next</p>
-						<p className='text-black' style={{ fontSize: 22 }}>{navigationData.next.title}</p>
+						<p key={`next_title`} className='text-black' style={{ fontSize: 14 }}>Next</p>
+						<p key={`next_content`} className='text-black' style={{ fontSize: 22 }}>{navigationData.next.title}</p>
 					</div>
 					<p className='text-black' style={{ fontSize: 22 }}>{`>`}</p>
 				</button>
@@ -108,65 +108,65 @@ const Litepaper = () => {
 	}
 
 	return (
-		<Layout pageClass={`litepaper_page`}>
-				<div className='litepaper_page_preview'>
-				</div>
-				<div className='litepaper_mainpage'>
-					<div className='page_sidebar' key={`page_sidebar`}>
-						<ul>
-							{
-								pages.length > 0 && pages.map((item, page_index) => (<li key={`page_list_${page_index}`}>
-									<a className='page_nav' onClick={() => getPageContent(item)}>{item.title}</a>
-								</li>))
-							}
-						</ul>
-					</div>
-					<div className='page_content' key={`page_content`}>
+		<Layout pageClass={`litepaper_page`} key={`litepaper_page`}>
+			<div className='litepaper_page_preview' key={`litepaper-preview-2374953293`}>
+			</div>
+			<div className='litepaper_mainpage' key={`litepaper-mainpage-23583735`}>
+				<div className='page_sidebar' key={`page-sidebar-583765744`}>
+					<ul>
 						{
-							content && <h1 className='page_content_title'>{content.title}</h1>
+							pages.length > 0 && pages.map((item, page_index) => (<li key={`page_list_${page_index}`}>
+								<a className='page_nav' onClick={() => getPageContent(item)}>{item.title}</a>
+							</li>))
 						}
-						{
-							content && content.document.nodes.map((item, content_index) => {
-								if (item.type === "paragraph") {
-									if (item.nodes[0].object === 'text' && item.nodes[0].leaves[0].marks.length > 0) {
-										if (item.nodes[0].leaves[0].marks[0].type === 'bold')
-											return <h3 className='page_content_subtitle' key={`content-title-${content_index}`}>{item.nodes[0].leaves[0].text}</h3>
-										else {
-											return <div key={`content-text-1-${content_index}`} style={{ margin: '10px 0' }}>{item.nodes[0].leaves.map((node, index) => <span style={node.marks.length > 0 ? { fontStyle: node.marks[0].type } : null}>{node.text}</span>)}</div>
-										}
-									} else if (item.nodes.length !== 3 && item.nodes[0].object === 'text' && !item.nodes[0].leaves[0].marks.length) {
-										return <p style={{ margin: '5px 0' }} key={`content-text-2-${content_index}`}>{item.nodes[0].leaves[0].text}</p>
-									} else if (item.nodes.length === 3) {
-										return <>{
-											item.nodes.map((node) => {
-												if (node.object === 'text') return <span key={`page_content_text_item_${node.leaves[0].text}`}>{node.leaves[0].text}</span>
-												else if (node.object === 'inline') return <div key={`page_content_link_item_${node.nodes[0].leaves[0].text}`}><a href={node.nodes[0].leaves[0].text}>{node.nodes[0].leaves[0].text}</a><br /></div>
-											})
-										}</>
+					</ul>
+				</div>
+				<div className='page_content'>
+					{
+						content && <h1 className='page_content_title'>{content.title}</h1>
+					}
+					{
+						content && content.document.nodes.map((item, content_index) => {
+							if (item.type === "paragraph") {
+								if (item.nodes[0].object === 'text' && item.nodes[0].leaves[0].marks.length > 0) {
+									if (item.nodes[0].leaves[0].marks[0].type === 'bold')
+										return <h3 className='page_content_subtitle' key={`content-title-${content_index}`}>{item.nodes[0].leaves[0].text}</h3>
+									else {
+										return <div key={`content-text-1-${content_index}`} style={{ margin: '10px 0' }}>{item.nodes[0].leaves.map((node, index) => <span key={`content-text-1-${content_index}-span-${index}`} style={node.marks.length > 0 ? { fontStyle: node.marks[0].type } : null}>{node.text}</span>)}</div>
 									}
-								} else if (item.type === "list-unordered") {
-									return <ul key={`page_content_list_ul_${content_index}`}>
-										{item.nodes.map((node, list_index) => (<li key={`page_content_list_li_${list_index}`}>
-											{
-												node.nodes[0].nodes[0].leaves.map((iitem, idx) => <span key={`page_content_list_li_span_${idx}`} style={iitem.marks.length > 0 ? { fontStyle: iitem.marks[0].type } : null}>{iitem.text}</span>)
-											}</li>))
-										}
-									</ul>
-								} else if (item.type === "images") {
-									getFileResource(org.id, item.nodes[0].data.ref.file)
-									return <figure key={item.nodes[0].data.ref.file}>
-										<img className='page_content_image' src={fileUrl} alt={item.nodes[0].fragments[0].nodes[0].nodes[0].leaves[0].text} />
-										<figcaption>{item.nodes[0].fragments[0].nodes[0].nodes[0].leaves[0].text}</figcaption>
-									</figure>
+								} else if (item.nodes.length !== 3 && item.nodes[0].object === 'text' && !item.nodes[0].leaves[0].marks.length) {
+									return <p style={{ margin: '5px 0' }} key={`content-text-2-${content_index}`}>{item.nodes[0].leaves[0].text}</p>
+								} else if (item.nodes.length === 3) {
+									return <>{
+										item.nodes.map((node) => {
+											if (node.object === 'text') return <span key={`page_content_text_item_${node.leaves[0].text}`}>{node.leaves[0].text}</span>
+											else if (node.object === 'inline') return <div key={`page_content_link_item_${node.nodes[0].leaves[0].text}`}><a href={node.nodes[0].leaves[0].text} style={{color: "orange"}}>{node.nodes[0].leaves[0].text}</a></div>
+										})
+									}</>
 								}
-							})
-						}
-						<div className='litepaper_bottom_navigation'>
-							{renderNavigationButtons()}
-						</div>
+							} else if (item.type === "list-unordered") {
+								return <ul key={`page_content_list_ul_${content_index}`}>
+									{item.nodes.map((node, list_index) => (<li key={`page_content_list_li_${list_index}`}>
+										{
+											node.nodes[0].nodes[0].leaves.map((iitem, idx) => <span key={`page_content_list_li_span_${idx}`} style={iitem.marks.length > 0 ? { fontStyle: iitem.marks[0].type } : null}>{iitem.text}</span>)
+										}</li>))
+									}
+								</ul>
+							} else if (item.type === "images") {
+								getFileResource(org.id, item.nodes[0].data.ref.file)
+								return <figure key={item.nodes[0].data.ref.file}>
+									<img className='page_content_image' src={fileUrl} alt={item.nodes[0].fragments[0].nodes[0].nodes[0].leaves[0].text} />
+									<figcaption>{item.nodes[0].fragments[0].nodes[0].nodes[0].leaves[0].text}</figcaption>
+								</figure>
+							}
+						})
+					}
+					<div className='litepaper_bottom_navigation' key={`navigation-buttons`}>
+						{renderNavigationButtons()}
 					</div>
 				</div>
-				<div id='mask' className='mask'><div>Loading...</div></div>
+			</div>
+			<div key={`litepaper-mask`} id='mask' className='mask'><div>Loading...</div></div>
 		</Layout>
 	)
 };
