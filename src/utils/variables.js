5cb21673095e45f49a614afddaa1b6de91c48418
V3 Solution <v3solutionsteam@gmail.com>
2024-06-10 19:23:23 +0200

    last push

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -1,5 +1,6 @@
 // const backend_url = 'http://127.0.0.1:5462'
-const backend_url = 'https://jobs3-backend.vercel.app/'
+const backend_url = 'http://192.168.100.25:5462/api/v1'
+// const backend_url = 'https://jobs3-backend.vercel.app/'
 
 module.exports = {
     backend_url
