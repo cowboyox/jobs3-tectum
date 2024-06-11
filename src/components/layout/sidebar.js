e2ac683663787ca3c1a42ab23b30a63915bc5924
stayForward09 <daveescott0059@gmail.com>
2024-06-11 07:42:32 -0500

    update toast

--- src/components/layout/sidebar.js
+++ src/components/layout/sidebar.js
@@ -3,6 +3,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import Link from 'next/link';
 
 const SideBar = () => {
+
     const [user, setUser] = useState({
         email: "",
         name: "",
@@ -12,14 +13,7 @@ const SideBar = () => {
     useEffect(() => {
         let tmp = localStorage.getItem('jobs_2024_token');
         if (tmp === null) {
-            toast({
-                variant: "destructive",
-                title: <h1 className='text-center'>Error</h1>,
-                description: <h3>Please Login First</h3>,
-                className: "bg-red-500 rounded-xl absolute top-[-94vh] xl:w-[10vw] md:w-[20vw] sm:w-[40vw] xs:[w-40vw] right-0 text-center"
-            });
-            alert("Please Login First");
-            router.push('/');
+            console.log("Please Login First");
         } else {
             setUser(JSON.parse(tmp).data.user);
         }
