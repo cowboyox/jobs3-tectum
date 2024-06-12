499714038355c7505e4499938437473afa25848a
Mr-selmani <selmani.abdelmoula.ma@gmail.com>
2024-06-12 02:58:14 +0100

    Modified portfolio empty page

--- src/app/(pages)/dashboard/[username]/freelancer/portfolio/create/page.js
+++ src/app/(pages)/dashboard/[username]/freelancer/portfolio/create/page.js
@@ -1,29 +1,19 @@
-import React from 'react';
-import { StepProvider, StepNumber, NextStep, PrevStep } from '@/components/layout/StepContext';
+import React from 'react'; 
 
 /*
- * Selmani: i abandoned this page in it's beggining of the progress lol
+ - Selmani: i abandoned this page in it's beggining of the progress lol
  * That's cause i have now to finish the gig page first, but if somehow  
-   you need to create it, feel free to use this code
+ * you need to create it, feel free to use the FormStep folder on the components
  
- * NextStep: is the button for when the user navigating through the gig steps
- * PrevStep: same thing as next, but for previous
- * StepNumbe: i was planning to use it to toggle the active class for 
-   the top navof the form
-
- * Don't use "use client", if you don't know how, please just text me and 
-   i will give more details
+ - For now i delted everything here
+ 
+ - NOTE: Better not to use "use client", if you don't know how, please just text me and 
+ * i will give more details
 */
 
 const CreatePortfolio = () => {
     return (
-        <StepProvider>
-            <div className='flex'>
-                <PrevStep />
-                <StepNumber />
-                <NextStep />
-            </div>
-        </StepProvider>
+        <></> 
     );
 };
 
