e9f999a43f76e3988bacf42d4eae6b6fee187a40
yaroslav <hy99aa@gmail.com>
2024-06-13 18:24:08 +0800

    add ai search UI into order page

--- src/app/(pages)/dashboard/[username]/client/freelancers/page.js
+++ src/app/(pages)/dashboard/[username]/client/freelancers/page.js
@@ -26,6 +26,7 @@ import { FaX, FaEllipsis, FaBan } from "react-icons/fa6";
 import { Separator } from "@/components/ui/seperator";
 import api from "@/utils/api";
 import searchOptions from "./searchOptions";
+import CustomIconDropdown from "@/components/UI/dropdown";
 
 const Freelancers = () => {
   const router = useRouter();
@@ -43,14 +44,10 @@ const Freelancers = () => {
     "FullTime",
   ]);
   const [freelancers, setFreelancers] = useState([1, 2, 3]);
-  const [openDropDown, setOpenDropDown] = useState(false);
-  const [selectedOption, setSelectedOption] = useState(searchOptions[0]);
+  const [searchType, setSearchType] = useState(searchOptions[0]);
   const [isSmallScreen, setIsSmallScree] = useState(false);
 
-  const handleSearchOptionClick = (v) => {
-    setOpenDropDown(false);
-    setSelectedOption(v);
-  };
+  const handleSearchTypeChange = (v) => setSearchType(v);
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth < 768) {
@@ -73,56 +70,17 @@ const Freelancers = () => {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
-  console.log(openDropDown, "open");
+
   return (
     <div className="p-0 sm:p-0 xl:mt-8 lg:mt-8">
       <div className="flex flex-row justify-between items-center bg-[#10191D] p-3 rounded-xl gap-5">
         <div className="flex flex-1 items-center ml-3 gap-3">
-          <div className="relative">
-            <div
-              className={
-                "flex flex-row gap-2 items-center p-2 bg-[#1B272C] rounded-[10px] "
-              }
-              onClick={() => setOpenDropDown((prev) => !prev)}
-            >
-              {selectedOption?.icon}
-              <svg
-                width="12"
-                height="6"
-                viewBox="0 0 12 6"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M11.1426 0.759766L7.02642 4.87593C6.54031 5.36204 5.74485 5.36204 5.25874 4.87593L1.14258 0.759766"
-                  stroke="#96B0BD"
-                  strokeWidth="1.5"
-                  strokeMiterlimit="10"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </div>
-
-            {/* Dropdown menu */}
-            <div
-              className={`absolute w-full top-full right-0 mt-1 bg-[#1B272C] rounded-[10px]  shadow-md ${
-                openDropDown ? "visible" : "hidden"
-              }`}
-            >
-              <div className="flex flex-col gap-2 items-start">
-                {searchOptions.map((v, i) => (
-                  <div
-                    className="flex items-center gap-2 hover:bg-[#2B3942] p-2 rounded-[5px] cursor-pointer w-full"
-                    key={i}
-                    onClick={() => handleSearchOptionClick(v)}
-                  >
-                    {v?.icon}
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
+          <CustomIconDropdown
+            options={searchOptions}
+            value={searchType}
+            optionLabel={"icon"}
+            onChange={handleSearchTypeChange}
+          />
           <input
             type="text"
             placeholder={isSmallScreen ? "" : "Search for keywords"}
