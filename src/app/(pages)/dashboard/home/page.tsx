a9bc11e9bbc3215f41d0e7fc6a751536d4f10896
umarhayat9855 <umarhayat9855@gmail.com>
2024-06-15 15:04:45 +0500

    resolve all issue is assign to me 01

--- src/app/(pages)/dashboard/home/page.tsx
+++ src/app/(pages)/dashboard/home/page.tsx
@@ -3,16 +3,17 @@ import Gigs from '@/components/dashboard/home/Gigs'
 import RecentlyViewed from '@/components/dashboard/home/RecentlyViewed'
 import Stats from '@/components/dashboard/home/Stats'
 import Hero from '@/components/dashboard/home/hero'
-import React from 'react'
+import React, { useState } from 'react';
 
 const page = () => {
+  const [search, setSearch] = useState('');
   return (
     <div className='py-10 w-full min-h-screen flex flex-col items-center'>
       <Hero />
       <div className='2xl:max-w-[1000px]'>
-        <Stats />
+        <Stats search={search} setSearch={setSearch} />
         <FillProfile />
-        <RecentlyViewed/>
+        <RecentlyViewed search={search} setSearch={setSearch}/>
         <Gigs/>
       </div>
     </div>
