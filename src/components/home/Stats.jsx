4162775e751050319a9b33a8a333944d4244bc05
stayForward09 <daveescott0059@gmail.com>
2024-06-14 19:33:58 -0500

    client/home hired Freelancers

--- src/components/home/Stats.jsx
+++ src/components/home/Stats.jsx
@@ -142,7 +142,7 @@ const Stats = () => {
                         </div>
                         <div className='flex flex-col justify-between gap-2 flex-1'>
                             {
-                                recentHires.map((spend,index) => (
+                                recentHires.length ? recentHires.map((spend,index) => (
                                     <div key={index} className='flex px-3 gap-1 items-center flex-1 bg-darkGray rounded-2xl'>
 
                                         <div className='flex-1 flex justify-center items-center'>
@@ -158,7 +158,8 @@ const Stats = () => {
                                             </div>
                                         </div>
                                     </div>
-                                ))
+                                )) : 
+                                <div className='text-center mt-[50%]'>Nothing yet</div>
                             }
                         </div>
                     </div>
