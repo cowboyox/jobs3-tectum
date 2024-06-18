0a433933f1623d59209ef875e48d45e3280d708a
smartguardox <4316278+smartguardox@users.noreply.github.com>
2024-06-19 01:13:06 +0900

    implemented gigs page for freelancer

--- src/app/(pages)/dashboard/[username]/freelancer/my-gigs/page.js
+++ src/app/(pages)/dashboard/[username]/freelancer/my-gigs/page.js
@@ -1,11 +1,27 @@
-import React from 'react'
+'use client';
+
+import React from 'react';
+import { useRouter } from "next/navigation";
 
 const GigsPage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const router = useRouter();
 
-export default GigsPage
+  const onCreate = () => {
+    router.push('./my-gigs/create');
+  }
+
+  return (
+    <div className='flex items-center justify-center flex-col gap-3 h-full mt-10'>
+      <h2 className='text-3xl font-bold'>Nothing Here Yet</h2>
+      <p className='text-[18px] text-slate-600'>Create your first Gig</p>
+      <div
+        className='cursor-pointer bg-[#DC4F13] py-2 px-20 rounded-xl transition hover:bg-white hover:text-black'
+        onClick={onCreate}
+      >
+        Create
+      </div>
+    </div>
+  );
+};
+
+export default GigsPage;
