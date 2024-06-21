6f293a4100bc121059da4cc898a8feba090fc4cd
Umarhayat9855 <umarhayat9855@gmail.com>
2024-06-22 02:20:23 +0500

    resolve conflict 02

--- next.config.js
+++ next.config.js
@@ -1,23 +1,23 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-	reactStrictMode: false,
-	sassOptions: {
-		fiber: false,
-		includePaths: [path.join(__dirname, 'styles')],
-	},
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "main.jobs3.io",
-				port: "",
-				pathname: "/**",
-			},
-		],
-		domains: ['localhost', '127.0.0.1', 'jobs3-backend.vercel.app']
-	},
-	eslint: {
+  reactStrictMode: false,
+  sassOptions: {
+    fiber: false,
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'main.jobs3.io',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+    domains: ['localhost', '127.0.0.1', 'jobs3-backend.vercel.app'],
+  },
+  eslint: {
     ignoreDuringBuilds: true,
-  }
-}
+  },
+};
