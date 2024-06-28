cb11da31489d8e4c3d9e85166f98667c00b2cd31
cowboyox <4316278+cowboyox@users.noreply.github.com>
2024-06-29 04:39:06 +0800

    fixed freelancer profile page error

--- src/components/layout/sidebar.js
+++ src/components/layout/sidebar.js
@@ -53,7 +53,7 @@ const SideBar = () => {
       name: 'Home',
     },
     {
-      href: `/dashboard/freelancer/profile`,
+      href: `/dashboard/freelancer/profile/${auth.currentProfile?.userId}`,
       icon: (
         <svg
           fill='none'
