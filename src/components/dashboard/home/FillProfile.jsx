912f50694227ea332000e2587b4db87b5cba3078
smartguardox <4316278+smartguardox@users.noreply.github.com>
2024-06-29 01:53:20 +0900

    removed user

--- src/components/dashboard/home/FillProfile.jsx
+++ src/components/dashboard/home/FillProfile.jsx
@@ -5,6 +5,7 @@ import React from 'react';
 
 import { Button } from '@/components/ui/button';
 import { useCustomContext } from '@/context/use-custom';
+import { USER_ROLE } from '@/utils/constants';
 
 const FillProfile = () => {
   const auth = useCustomContext();
