ec9cde3387d48526bb20a9dde289136e822d8947
cowboyox <4316278+cowboyox@users.noreply.github.com>
2024-07-01 14:17:50 -0400

    fix: Api integration on order detail page and dynamic avatar in header

--- src/app/(pages)/dashboard/client/orders/[id]/page.js
+++ src/app/(pages)/dashboard/client/orders/[id]/page.js
@@ -1,5 +1,6 @@
 'use client';
 import React from 'react';
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Form } from '@/components/ui/form';
 import { useForm } from 'react-hook-form';
@@ -14,32 +15,38 @@ import { PiLineVerticalLight } from "react-icons/pi";
 import { GoCircle } from "react-icons/go";
 import { FaPlus } from "react-icons/fa6";
 import { CiCircleQuestion } from "react-icons/ci";
-
+import { useCustomContext } from '@/context/use-custom';
+import { useGetClientGigContractById } from '@/hooks/useGetClientGigContractById';
 
 const MessagesArea = ()=> {
+  const auth = useCustomContext();
+
   return (
     <PanelContainer>
       <div className="flex flex-col gap-4">
         <div className="flex space-between">
-          <div className="w-1/2 mobile:w-7/12 flex gap-5 mobile:gap-2">
-            <img src='/assets/images/users/user-1.png' className='rounded-full w-12 h-12 mobile:h-8 mobile:w-8 object-cover aspect-square' />
-            <div className="flex items-center gap-2 mobile:gap-1 cursor-pointer">
+          <div className="flex w-1/2 gap-5 mobile:w-7/12 mobile:gap-2">
+            <img 
+              src={auth?.currentProfile?.avatarURL ? auth?.currentProfile?.avatarURL : '/assets/images/users/user-1.png'}
+              className='object-cover w-12 h-12 rounded-full mobile:h-8 mobile:w-8 aspect-square'
+            />
+            <div className="flex items-center gap-2 cursor-pointer mobile:gap-1">
               <span className='text-base mobile:text-xs'>Use a quick response</span>
               <FaAngleDown />
             </div>
           </div>
-          <div className="w-1/2 mobile:w-5/12 flex gap-3 items-center justify-end"> 
+          <div className="flex items-center justify-end w-1/2 gap-3 mobile:w-5/12"> 
             <span className='text-base mobile:text-xs text-[#96B0BD]'>
               Local time:
             </span>
-            <span className='text-base mobile:text-xs text-white'>
+            <span className='text-base text-white mobile:text-xs'>
               4:25 pm
             </span>
           </div>
         </div>
         <textarea className='border border-[#526872] p-5 h-80 bg-transparent rounded-2xl text-white' placeholder='Type your message here...'/>
         <div className="flex gap-3 p-2 rounded-2xl bg-[#1B272C]">
-          <div className="w-full rounded-2xl  text-white py-5 mobile:py-3 text-center hover:bg-white hover:text-black transition cursor-pointer">
+          <div className="w-full py-5 text-center text-white transition cursor-pointer rounded-2xl mobile:py-3 hover:bg-white hover:text-black">
             Back
           </div>
           <div className="w-full rounded-2xl bg-[#DC4F13] text-white py-5 mobile:py-3 text-center cursor-pointer">
@@ -51,17 +58,35 @@ const MessagesArea = ()=> {
   )
 }
 const OrderPage = ({ params }) => {
+  const { data: contractRawData } = useGetClientGigContractById(
+    params.id
+  );
+  const contractInfo = contractRawData?.data?.data;
+  
+  const startDateString = contractInfo?.contractStartDate;
+  const date = new Date(startDateString);
+  const options = {
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+  };
+
+  const formattedStartDate = date.toLocaleString('en-US', options); // Output: "Jun 26, 2024, 5:23 PM"
+
   const form = useForm();
   return (
-    <Tabs defaultValue="details" className="w-full flex flex-col gap-6">
+    <Tabs defaultValue="details" className="flex flex-col w-full gap-6">
         <TabsList className='w-full h-auto p-0 bg-transparent'>
             <TabsTrigger 
-              className='w-full py-6 text-base bg-transparent border-b-2 border-[#516170] data-[state=active]:border-[#dc4f14]' 
+              className='w-full py-6 text-base bg-transparent border-b-4 border-[#516170] data-[state=active]:border-[#dc4f14]' 
               value="details">
                 Details
             </TabsTrigger>
             <TabsTrigger 
-              className='w-full py-6 text-base bg-transparent border-b-2 border-[#516170] data-[state=active]:border-[#dc4f14]' 
+              className='w-full py-6 text-base bg-transparent border-b-4 border-[#516170] data-[state=active]:border-[#dc4f14]' 
               value="requirements">
                 Requirements
             </TabsTrigger>
@@ -69,38 +94,38 @@ const OrderPage = ({ params }) => {
         <div className="flex gap-8 mobile:flex-col">
           <div className="w-8/12 mobile:w-full">
             <TabsContent value="details" className='w-full'>
-              <div className="w-full flex flex-col gap-5">
+              <div className="flex flex-col w-full gap-5">
                 <PanelContainer>
                   <div className="text-[24px] text-white mobile:text-xl">
-                    Digital interface for finance project
+                    {contractInfo?.clientGig?.gigTitle}
                   </div>
-                  <div className='flex w-full items-center gap-2 md:w-3/4 md:gap-5'>
+                  <div className='flex items-center w-full gap-2 md:w-3/4 md:gap-5'>
                     <div className='relative w-12 h-12 mobile:h-8 mobile:w-8'>
                       <img
-                        className='aspect-square h-full w-full rounded-full object-cover'
-                        src='/assets/images/users/user-3.png'
+                        className='object-cover w-full h-full rounded-full aspect-square'
+                        src={contractInfo?.freelancer?.avatarURL ? contractInfo?.freelancer?.avatarURL : '/assets/images/users/user-3.png'}
                       /> 
-                      <div className='absolute bottom-1 mobile:bottom-0 right-1 mobile:right-0 h-3 w-3 mobile:h-3 mobile:w-3 rounded-full bg-green-500' />
+                      <div className='absolute w-3 h-3 bg-green-500 rounded-full bottom-1 mobile:bottom-0 right-1 mobile:right-0 mobile:h-3 mobile:w-3' />
                     </div>
                     <div className='flex flex-col gap-4'>
                       <div className='flex items-center gap-2'>
-                        <h2 className='text-xl mobile:text-xs'>Emily Rosenberg</h2>
-                        <img className='h-4 w-4 mobile:h-3 mobile:w-3' src='/assets/images/icons/checkmark.svg' />
+                        <h2 className='text-xl mobile:text-xs'>{contractInfo?.freelancer?.fullName}</h2>
+                        <img className='w-4 h-4 mobile:h-3 mobile:w-3' src='/assets/images/icons/checkmark.svg' />
                       </div>
                     </div>
                     <div className='flex items-center gap-3'>
                       <IoCalendarOutline className='h-5 w-5 mobile:h-3 mobile:w-3 fill-[#96B0BD] stroke-[#96B0BD]' />
                       <p className='text-[15px] text-[#96B0BD] mobile:text-xs'>
-                        Jun 5, 2022, 3:13 PM
+                        {formattedStartDate}
                       </p>
                     </div>
                   </div>
                   <PanelContainer nested={true}>
-                    <div className="flex space-between gap-4 items-center">
+                    <div className="flex items-center gap-4 space-between">
                       <span className='text-base mobile:text-xs text-[#96B0BD]'>Order number</span>
-                      <span className='text-base mobile:text-xs text-white font-bold'>#{params.id}</span>
+                      <span className='text-base font-bold text-white mobile:text-xs'>#{params.id}</span>
                       <div className="py-1 px-2 text-sm border border-[#1BBF36] text-[#1BBF36] rounded-xl ml-auto mobile:text-xs">
-                        Active
+                        {contractInfo?.status}
                       </div>
                     </div>
                   </PanelContainer>
@@ -112,7 +137,7 @@ const OrderPage = ({ params }) => {
                             <span className='text-base mobile:text-xs text-[#96B0BD]'>
                               Item
                             </span>
-                            <span className='text-base mobile:text-xs text-white font-bold'>
+                            <span className='text-base font-bold text-white mobile:text-xs'>
                               Design UI/UX design for your mobile apps
                             </span>
                           </div>
@@ -122,26 +147,26 @@ const OrderPage = ({ params }) => {
                             <span className='text-base mobile:text-xs text-[#96B0BD]'>
                               Duration
                             </span>
-                            <span className='text-base mobile:text-xs text-white font-bold'>
+                            <span className='text-base font-bold text-white mobile:text-xs'>
                               2 days
                             </span>
                           </div>
                         </div>
                         <div className="w-2/12">
-                          <div className="flex flex-col gap-2 items-end">
+                          <div className="flex flex-col items-end gap-2">
                             <span className='text-base mobile:text-xs text-[#96B0BD]'>
                               Price
                             </span>
-                            <span className='text-base mobile:text-xs text-white font-bold'>
-                              $400
+                            <span className='text-base font-bold text-white mobile:text-xs'>
+                              ${contractInfo?.clientGig?.gigPrice}
                             </span>
                           </div>
                         </div>
                       </div>
                       <div className="border-t border-[#28373E] w-full"/>
                       <div className="p-4 bg-[#10191D] rounded-xl flex justify-between">
-                        <span className='text-base text-white font-bold'>Total</span>
-                        <span className='text-base text-white font-bold'>$400</span>
+                        <span className='text-base font-bold text-white'>Total</span>
+                        <span className='text-base font-bold text-white'>${contractInfo?.clientGig?.gigPrice}</span>
                       </div>
                     </div>
                   </PanelContainer>
@@ -155,7 +180,7 @@ const OrderPage = ({ params }) => {
                 <Form {...form}>
                   <form>
                     <PanelContainer>
-                      <span className='text-2xl mobile:text-xl text-white font-bold mobile:font-normal'>
+                      <span className='text-2xl font-bold text-white mobile:text-xl mobile:font-normal'>
                         Brief about the project
                       </span>
                       <textarea className='border border-[#526872] p-5 h-80 bg-transparent rounded-2xl text-white'/>
@@ -177,15 +202,15 @@ const OrderPage = ({ params }) => {
                 </Form>
             </TabsContent>
           </div>
-          <div className="w-4/12 mobile:w-full flex flex-col gap-5">
+          <div className="flex flex-col w-4/12 gap-5 mobile:w-full">
             <PanelContainer>
-              <span className='text-xl text-white font-bold'>Time left</span>
+              <span className='text-xl font-bold text-white'>Time left</span>
               <div className="flex gap-2">
                 <div className="bg-[#1B272C] w-full py-2 rounded-2xl flex flex-col gap-1 items-center">
                   <span className='text-sm text-[#96B0BD]'>
                     Days
                   </span>
-                  <span className='text-xl text-white font-bold'>
+                  <span className='text-xl font-bold text-white'>
                     01
                   </span>
                 </div>
@@ -193,7 +218,7 @@ const OrderPage = ({ params }) => {
                   <span className='text-sm text-[#96B0BD]'>
                     Hours
                   </span>
-                  <span className='text-xl text-white font-bold'>
+                  <span className='text-xl font-bold text-white'>
                     11
                   </span>
                 </div>
@@ -201,7 +226,7 @@ const OrderPage = ({ params }) => {
                   <span className='text-sm text-[#96B0BD]'>
                     Minutes
                   </span>
-                  <span className='text-xl text-white font-bold'>
+                  <span className='text-xl font-bold text-white'>
                     06
                   </span>
                 </div>
@@ -209,7 +234,7 @@ const OrderPage = ({ params }) => {
                   <span className='text-sm text-[#96B0BD]'>
                     Seconds
                   </span>
-                  <span className='text-xl text-white font-bold'>
+                  <span className='text-xl font-bold text-white'>
                     52
                   </span>
                 </div>
@@ -217,15 +242,15 @@ const OrderPage = ({ params }) => {
             </PanelContainer>
             <PanelContainer>
               <div className="flex flex-col gap-5">
-              <span className='text-xl text-white font-bold'>Track Order</span>
-                <div className="flex gap-3 items-center"> 
+              <span className='text-xl font-bold text-white'>Track Order</span>
+                <div className="flex items-center gap-3"> 
                   <IoIosCheckmarkCircle size={30} fill="#DC4F13"/>
                   <span className='text-base text-[#96B0BD] font-bold'>
                     Order Placed
                   </span>
                 </div>
                 <PiLineVerticalLight size={30} fill="#DC4F13" className="-my-3"/>
-                <div className="flex gap-3 items-center"> 
+                <div className="flex items-center gap-3"> 
                   <GoCircle size={30} fill="#DC4F13"/>
                   <span className='text-base text-[#F5F5F5] font-bold'>
                     Submit requirements
@@ -235,7 +260,7 @@ const OrderPage = ({ params }) => {
             </PanelContainer>
             <PanelContainer>
               <div className="flex justify-between">
-                <span className='text-lg text-white font-bold'>
+                <span className='text-lg font-bold text-white'>
                   Private Note
                 </span>
                 <div className='flex items-center gap-2 cursor-pointer'>
@@ -248,7 +273,7 @@ const OrderPage = ({ params }) => {
               </span>
             </PanelContainer>
             <PanelContainer>
-              <span className='text-lg text-white font-bold'>
+              <span className='text-lg font-bold text-white'>
                 Support
               </span>
               <div className="flex gap-3 pb-4 border-b border-[#1B272C]">
@@ -256,7 +281,7 @@ const OrderPage = ({ params }) => {
                   <CiCircleQuestion size={25} fill='#8599A5' />
                 </div>
                 <div className="flex flex-col gap-1">
-                  <span className='text-lg text-white font-bold'>FAQs</span>
+                  <span className='text-lg font-bold text-white'>FAQs</span>
                   <span className='text-base text-[#96B0BD]'>Find needed answers</span>
                 </div>
               </div>
@@ -265,7 +290,7 @@ const OrderPage = ({ params }) => {
                   <CiCircleQuestion size={25} fill='#8599A5' />
                 </div>
                 <div className="flex flex-col gap-1">
-                  <span className='text-lg text-white font-bold'>Resolution Center</span>
+                  <span className='text-lg font-bold text-white'>Resolution Center</span>
                   <span className='text-base text-[#96B0BD]'>Find needed answers</span>
                 </div>
               </div>
