26a51b72b418fe0838631486565c9f1fea3ecbe3
cowboyox <4316278+cowboyox@users.noreply.github.com>
2024-07-04 01:09:06 -0400

    feat: Applying to freelancers's gig feature

---
+++ src/hooks/useGetAllClientOrdersProposed.js
@@ -0,0 +1,70 @@
+import { useQuery } from '@tanstack/react-query';
+
+import api from '@/utils/api';
+
+export const useGetAllClientOrdersProposed = (profileId) => {
+  return useQuery({
+    cacheTime: Infinity,
+    enabled: !!profileId,
+    queryFn: async () => {
+      if (profileId) {
+        try {
+          const result = await api.get(`/api/v1/freelancer_gig/find_all_orders_of_client/${profileId}`);
+          console.log("result in useGetAllClientGigsProposed:", result);
+          const proposals = [];
+          const lives = [];
+
+          if (result.data.proposals.length > 0) {
+            result.data.proposals.map((proposal) => {
+              proposals.push({
+                creator: {
+                  fullName: proposal.proposer.fullName,
+                },
+                freelancerId: proposal.proposer,
+                gigDescription: proposal.clientGig.gigDescription,
+                gigId: proposal.clientGig._id,
+                gigPostDate: proposal.clientGig.gigPostDate,
+                gigPrice: proposal.clientGig.gigPrice,
+                gigTitle: proposal.clientGig.gigTitle,
+                maxBudget: proposal.clientGig.maxBudget,
+                minBudget: proposal.clientGig.minBudget,
+                walletPubkey: proposal.proposer?.walletPubkey,
+              });
+            });
+          }
+
+          if (result.data.contracts.length > 0) {
+            result.data.contracts.map((contract) => {
+              lives.push({
+                id: contract._id,
+                creator: {
+                  fullName: contract.proposer.fullName,
+                },
+                freelancerId: contract.proposer,
+                gigDescription: contract.clientGig.gigDescription,
+                gigId: contract.clientGig._id,
+                gigPostDate: contract.clientGig.gigPostDate,
+                gigPrice: contract.clientGig.gigPrice
+                  ? `$${contract.clientGig.gigPrice}`
+                  : `$${contract.clientGig.minBudget}/hr ~ $${contract.clientGig.maxBudget}/hr`,
+                gigTitle: contract.clientGig.gigTitle,
+                status: contract.status,
+                contractId: contract.contractId,
+              });
+            });
+          }
+
+          return { lives, proposals };
+        } catch (e) {
+          console.error(e);
+
+          return null;
+        }
+      } else {
+        return null;
+      }
+    },
+    queryKey: ['useGetAllClientOrdersProposed', profileId],
+    staleTime: Infinity,
+  });
+};
