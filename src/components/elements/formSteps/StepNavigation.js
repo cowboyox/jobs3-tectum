61caf5253d97bd51df42c3ad286a96d1b0ad4492
Mr-selmani <selmani.abdelmoula.ma@gmail.com>
2024-07-05 00:13:29 +0100

    Added Create portfolio page

--- src/components/elements/formSteps/StepNavigation.js
+++ src/components/elements/formSteps/StepNavigation.js
@@ -4,12 +4,14 @@ import { NextStep, PrevStep } from '@/components/elements/formSteps/StepContext'
 
 const FormNavigation = () => {
   return (
-    <div className='flex items-center justify-end gap-7'>
-      <PrevStep className='cursor-pointer text-base text-slate-500 transition hover:text-white' />
-      <NextStep
-        className='cursor-pointer rounded-xl bg-[#DC4F13] px-12 py-2 transition hover:bg-white hover:text-black'
-        max={6}
-      />
+    <div className='w-full flex items-center justify-end gap-7'>
+      <div className="flex w-full gap-3 p-2 rounded-2xl bg-[#1B272C]">
+        <PrevStep className='w-full py-5 text-center text-white transition cursor-pointer rounded-2xl mobile:py-3 hover:bg-white hover:text-black' />
+        <NextStep
+          className='w-full rounded-2xl bg-[#DC4F13] text-white py-5 mobile:py-3 text-center cursor-pointer'
+          max={6}
+        />
+      </div>
     </div>
   );
 };
