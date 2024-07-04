7947417e9e2fa05551f0c463fbfd356e285c2d74
cowboyox <4316278+cowboyox@users.noreply.github.com>
2024-07-04 09:21:44 -0400

    feat: Client order accept and reject feature

--- src/app/(pages)/dashboard/freelancer/offer/page.jsx
+++ src/app/(pages)/dashboard/freelancer/offer/page.jsx
@@ -42,18 +42,62 @@ const Offer = () => {
           <div className="w-full">
             <TabsContent value="Accepted" className='w-full'>
               <div className="flex flex-col w-full gap-5">
-                <OfferItem/>
-                <OfferItem/>
-                <OfferItem/>
-                <OfferItem/>
+              {
+                lives.length > 0 ? 
+                (
+                  <>
+                    {lives.map((order, index) => (
+                      <OfferItem 
+                        proposalId={order.proposalId} 
+                        gigTitle={order.gigTitle} 
+                        gigPrice={order.gigPrice} 
+                        deliveryTime={order.deliveryTime} 
+                        proposal={order.proposal} 
+                        refetchAllOrdersProposed={refetchAllOrdersProposed}
+                        accepted={true}
+                      />
+                    ))}
+                    <button className='mt-6 w-full border border-[#28373E] p-3 text-center'>
+                      Load more +{' '}
+                    </button>
+                  </>
+                ) : (
+                  <div className='flex flex-col items-center justify-center h-full gap-3 py-20'>
+                    <h2 className='text-3xl font-bold'>Nothing Here Yet</h2>
+                    <p className='text-[18px] text-slate-600'>Accepted proposals will be here</p>
+                  </div>
+                )
+              }
               </div>
             </TabsContent>
             <TabsContent value="proposals">
               <div className='flex flex-col w-full gap-5'>
-                <OfferItem/>
-                <OfferItem/>
-                <OfferItem/>
-                <OfferItem/>
+              {
+                proposals.length > 0 ? 
+                (
+                  <>
+                    {proposals.map((proposal, index) => (
+                      <OfferItem 
+                        proposalId={proposal.proposalId} 
+                        gigTitle={proposal.gigTitle} 
+                        gigPrice={proposal.gigPrice} 
+                        deliveryTime={proposal.deliveryTime} 
+                        proposal={proposal.proposal}
+                        refetchAllOrdersProposed={refetchAllOrdersProposed}
+                        accepted={false}
+                      />
+                    ))}
+                    <button className='mt-6 w-full border border-[#28373E] p-3 text-center'>
+                      Load more +{' '}
+                    </button>
+                  </>
+                ) : (
+                  <div className='flex flex-col items-center justify-center h-full gap-3 py-20'>
+                    <h2 className='text-3xl font-bold'>Nothing Here Yet</h2>
+                    <p className='text-[18px] text-slate-600'>Proposals will be here</p>
+                  </div>
+                )
+              }
               </div>
             </TabsContent>
           </div>
