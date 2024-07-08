0f80f4c6791f12932ab19756ff2d39eab92d768b
smartguardox <4316278+smartguardox@users.noreply.github.com>
2024-07-09 00:00:08 +0900

    fixed page style

--- src/app/(pages)/dashboard/client/job-application/[gigId]/page.jsx
+++ src/app/(pages)/dashboard/client/job-application/[gigId]/page.jsx
@@ -1,15 +1,19 @@
 'use client';
 
-import { usePathname, useRouter } from 'next/navigation';
+import { useParams, usePathname, useRouter } from 'next/navigation';
 import React, { useEffect } from 'react';
 
 import GigPage from './GigPage';
 
-import api from '@/utils/api';
+import { useGetFreelancerGigById } from '@/hooks/useGetFreelancerGigById';
 
-const Page = async ({ params }) => {
+const Page = () => {
   const router = useRouter();
   const pathname = usePathname();
+  const { gigId } = useParams();
+  const { data: gigData } = useGetFreelancerGigById(gigId);
+
+  console.log(gigData);
 
   useEffect(() => {
     let tmp = localStorage.getItem('jobs_2024_token');
@@ -18,8 +22,7 @@ const Page = async ({ params }) => {
     }
   }, [router, pathname]);
 
-  const gigData = await getGigById(params.gigId);
-  const { gigTitle, gigPostDate, gigPrice, creator: freelancer, walletPubkey } = gigData.data.data;
+  const { gigTitle, gigPostDate, gigPrice, creator: freelancer, walletPubkey } = gigData.data;
 
   return (
     <GigPage
@@ -32,9 +35,5 @@ const Page = async ({ params }) => {
     />
   );
 };
-export default Page;
 
-const getGigById = async (gigId) => {
-  const resData = await api.get(`/api/v1/freelancer_gig/get_gig_by_id/${gigId}`);
-  return resData;
-};
+export default Page;
