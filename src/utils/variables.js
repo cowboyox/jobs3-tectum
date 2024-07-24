e07ec2821b59a89fb3cfd0362f08848c934dad05
cowboyox <4316278+cowboyox@users.noreply.github.com>
2024-07-24 11:30:07 -0400

    feat: Add main.yml

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -1,5 +1,6 @@
 // const backend_url = 'http://127.0.0.1:5462'
-const backend_url = 'https://jobs3-backend.vercel.app';
+// const backend_url = 'https://jobs3-backend.vercel.app';
+const backend_url = 'https://backend.jobs3.io';
 
 module.exports = {
   backend_url,
