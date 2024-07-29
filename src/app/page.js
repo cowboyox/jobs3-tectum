ea39a84499d188014666e50e7a0358ced4784423
Mr-selmani <selmani.abdelmoula.ma@gmail.com>
2024-07-30 00:22:40 +0100

    Added .js extension on the home page sections imports

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,12 @@ import React from 'react';
 // Components
 import Layout from '@/components/layout/Layout';
 
-import HeroSection from '@/components/pages/home/hero';
-import GetPaid from '@/components/pages/home/get-paid';
-import ExploreSection from '@/components/pages/home/explore';
-import PerfectFor from '@/components/pages/home/perfect-for';
-import FAQ from '@/components/pages/home/faq';
-import ReferFirend from '@/components/pages/home/refer-firend';
+import HeroSection from '@/components/pages/home/hero.js';
+import GetPaid from '@/components/pages/home/get-paid.js';
+import ExploreSection from '@/components/pages/home/explore.js';
+import PerfectFor from '@/components/pages/home/perfect-for.js';
+import FAQ from '@/components/pages/home/faq.js';
+import ReferFirend from '@/components/pages/home/refer-firend.js';
 
 export default function Home() {
   return (
