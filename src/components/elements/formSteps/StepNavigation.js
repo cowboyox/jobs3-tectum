254b266376e386faf5f511fbf1b0f9f9ec97f0f4
cowboyox <4316278+cowboyox@users.noreply.github.com>
2024-07-31 05:52:20 +0800

    employee-profile-resume-build

--- src/components/elements/formSteps/StepNavigation.js
+++ src/components/elements/formSteps/StepNavigation.js
@@ -6,12 +6,15 @@ const FormNavigation = (props) => {
   return (
     <div className='flex w-full items-center justify-end gap-7'>
       <div className='flex w-full gap-3 rounded-2xl bg-[#1B272C] p-2'>
-        <PrevStep className='mobile:py-3 w-full cursor-pointer rounded-2xl py-5 text-center text-white transition hover:bg-white hover:text-black' />
+        <PrevStep className='mobile:py-3 w-full cursor-pointer rounded-2xl py-5 text-center text-white transition hover:bg-white hover:text-black' setStepNumbers={props.setStepNumbers} />
         <NextStep
           className='mobile:py-3 w-full cursor-pointer rounded-2xl bg-[#DC4F13] py-5 text-center text-white flex justify-center items-center gap-1'
           max={props.max ? props.max : 6}
           isAuth={props.isAuth}
           isWaiting={props.isWaiting}
+          setStepNumbers={props.setStepNumbers}
+          setIsResumePreview={props.setIsResumePreview}
+          finalButtonName={props.finalButtonName}
         />
       </div>
     </div>
